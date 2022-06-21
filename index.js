ff571db183f144cdf0754bb6d39424b1c8ffed1d
James G <stackz@protonmail.com>
2022-06-20 23:28:46 -0400

    added all prompts for engineer, intern, and manager

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+teamArray = [];
 
 const mainMenu = () => {
     inquirer.prompt({
@@ -6,19 +10,123 @@ const mainMenu = () => {
         name: "emptype",
         message: "What type of employee would you like to create?",
         choices: ["Engineer", "Intern", "Manager", "Done"]
-    }).then (answer =>{
+    }).then(answer => {
         console.log(answer);
         if (answer.emptype === "Engineer") {
-            console.log("Ask Engineer Questions");
+            askEngineer();
         } else if (answer.emptype === "Intern") {
             console.log("Ask Intern Questions");
         } else if (answer.emptype === "Manager") {
-           console.log("Ask Manager Questions"); 
-        } else {process.exit()}
+            askManager();
+        } else { process.exit() }
     })
 }
 
-// ask engineer function
-// ask manager function
-// ask intern fuction
+const askEngineer = () => {
+    inquirer.prompt ([
+    
+        {
+          type: "input",
+          name: "name",
+          message: "What is the engineer's name?"
+        },
+    
+        {
+          type: "input",
+          name: "employeeId",
+          message: "What is the engineer's employee ID number?"
+        },
+    
+        {
+          type: "input",
+          name: "email",
+          message: "What is the engineer's email address?"
+        },
+    
+        {
+          type: "input",
+          name: "github",
+          message: "What is the engineer's Github username?"
+        }
+    
+      ]).then(answers => {
+        const engineer = new Engineer(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+        teamArray.push(engineer);
+        mainMenu();
+      });
+    
+};
+
+const askManager = () => {
+    inquirer.prompt ([
+    
+        {
+          type: "input",
+          name: "name",
+          message: "What is the manager's name?"
+        },
+    
+        {
+          type: "input",
+          name: "employeeId",
+          message: "What is the manager's employee ID number?"
+        },
+    
+        {
+          type: "input",
+          name: "email",
+          message: "What is the manager's email address?"
+        },
+    
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the manager's office number?"
+        }
+    
+      ]).then(answers => {
+        const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+        teamArray.push(manager);
+        mainMenu();
+      });
+    
+};
+
+const askIntern = () => {
+    inquirer.prompt ([
+    
+        {
+          type: "input",
+          name: "name",
+          message: "What is the intern's name?"
+        },
+    
+        {
+          type: "input",
+          name: "employeeId",
+          message: "What is the intern's employee ID number?"
+        },
+    
+        {
+          type: "input",
+          name: "email",
+          message: "What is the intern's email address?"
+        },
+    
+        {
+          type: "input",
+          name: "github",
+          message: "What is the intern's Github username?"
+        }
+    
+      ]).then(answers => {
+        const intern = new Intern(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+        teamArray.push(intern);
+        mainMenu();
+      });
+    
+};
+
+
+
 mainMenu();
